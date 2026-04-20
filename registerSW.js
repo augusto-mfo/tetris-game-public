9e11d3d32cea750ee291797310d6d84ca8174ae0
Augusto Oliveira <42597472+augusto-mfo@users.noreply.github.com>
2026-04-20 23:16:13 +0100

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tetris-game-public/sw.js', { scope: '/tetris-game-public/' })})}
